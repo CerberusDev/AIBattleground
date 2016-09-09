03790d7246fd89c0f3ad179366af796dca15d743
Maciej Pryc <pryc.maciej@gmail.com>
2016-09-09 23:14:24 +0200

    First steps on BT implementation, base structs for Node, Composite and Task + two actual Tasks created.

--- AIBattleground/Src/AISystemBT.cpp
+++ AIBattleground/Src/AISystemBT.cpp
@@ -2,22 +2,26 @@
 // ------------- AI Battleground, Copyright(C) Maciej Pryc, 2016 --------------
 // ----------------------------------------------------------------------------
 
+#include <iostream>
+
 #include "AISystemBT.h"
 #include "Actor.h"
 #include "Blackboard.h"
 
 AISystemBT::AISystemBT(class Actor* argOwner, class Blackboard* argBlackboard) :
-AISystemBase(argOwner, argBlackboard)
+AISystemBase(argOwner, argBlackboard), Root(nullptr)
 {
-
+	BTComposite* FirstComposite = new BTComposite();
+	Root = FirstComposite;
+	FirstComposite->AddChild(new BTTask_GoTowardsNearestEnemy(Owner));
 }
 
 AISystemBT::~AISystemBT()
 {
-
+	delete Root;
 }
 
 void AISystemBT::Update()
 {
-
+	Root->Update();
 }
--- AIBattleground/Src/AISystemBT.h
+++ AIBattleground/Src/AISystemBT.h
@@ -4,10 +4,69 @@
 
 #pragma once
 
+#include <vector>
+
 #include "AISystemBase.h"
+#include "Actor.h"
 
 class AISystemBT : public AISystemBase
 {
+private:
+	struct BTNode
+	{
+		virtual void Update() = 0;
+	};
+
+	struct BTComposite : public BTNode
+	{
+		std::vector<BTNode*> Childs;
+
+		virtual void Update()
+		{
+			for (auto it = Childs.begin(); it != Childs.end(); ++it)
+				(*it)->Update();
+		}
+
+		void AddChild(BTNode* NewChild)
+		{
+			Childs.push_back(NewChild);
+		}
+
+		virtual ~BTComposite()
+		{
+			for (auto it = Childs.begin(); it != Childs.end(); ++it)
+				delete *it;
+		}
+	};
+
+	struct BTTask : public BTNode
+	{
+		Actor* OwningActor;
+		BTTask(Actor* argOwningActor) : OwningActor(argOwningActor) {};
+	};
+
+	struct BTTask_StopMovement : public BTTask
+	{
+		BTTask_StopMovement(Actor* argOwningActor) : BTTask(argOwningActor) {};
+
+		virtual void Update()
+		{
+			OwningActor->StopMovement();
+		}
+	};
+
+	struct BTTask_GoTowardsNearestEnemy : public BTTask
+	{
+		BTTask_GoTowardsNearestEnemy(Actor* argOwningActor) : BTTask(argOwningActor) {};
+
+		virtual void Update()
+		{
+			OwningActor->GoTowardsNearestEnemy();
+		}
+	};
+
+	BTNode* Root;
+
 public:
 	AISystemBT(class Actor* argOwner, class Blackboard* argBlackboard);
 	~AISystemBT();
